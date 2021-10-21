d78a49d1c86ba45bbf5f76f00462cd24cb14bd2f
jinwoo <tjdwnsdud3@naver.com>
2021-10-21 01:59:24 +0000

    update

--- configs/_base_/jinwoo/dataset.py
+++ configs/_base_/jinwoo/dataset.py
@@ -1,5 +1,5 @@
 # dataset settings
-dataset_type = 'COCOTrashDataset'
+dataset_type = 'COCOStuffDataset'
 data_root = "/opt/ml/segmentation/mmseg_data"
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
